79e0400cd9d20ade6916b27dee904a66fed5c025
Chris Pollock <cpollock911@gmail.com>
2021-07-19 16:35:58 -0700

    Added Second Essay

--- src/React/Homework/Homework.jsx
+++ src/React/Homework/Homework.jsx
@@ -31,7 +31,22 @@ const Homework = () => {
                 number={ 2 }
                 question='Explain the difference between HTML, CSS and JS.'
             >
-                My long winded Answer 2
+                <h2>HTML, CSS, and JavaScript</h2>
+                <p>
+                Programmers utilize HTML, CCS, and JavaScript to deal with their coding developments.   Each programming language has different purposes, making them necessary for a developer based on their project.  In this essay, I will be discussing the main differences between the three programming languages and how they are applicable.
+                </p>
+                <p>
+                HTML, or HyperText Markup Language, essentially provides the basic structure for websites.  HTML will control the overall formatting of the website, which includes document structure, embedding objects like images and interactive forms, and formatting text elements through tags.  In addition, HTML utilizes the tagging system to insert text, links, images, or any other information on the web page.  For example, if I were to showcase this essay on my web application, I would use HTML to format paragraphs and headings to make the content more readable.  HTML uses both CSS and JavaScript to enhance the style and behavior of web pages.
+                </p>
+                <p>
+                CSS, or Cascading Style Sheet, will stylize the website once establishing the foundation with HTML.  CSS modifies the design and display of existing HTML elements, which then are represented correctly on each web page.  CSS will allow you to design the colors, fonts, and layouts for your site.  In addition, CSS can help adapt your website to display correctly across various devices regardless of screen size.  To insert CSS, the programmer would use a selector followed by braces to form a declaration block.  The declaration block uses braces that surround a property element and a value element.  The properties deal with different styles and formats, while the values are specific to the property listed.  For example, if I wanted to modify my font size and color, I would use CSS to select which element and alter it through declaration blocks.
+                </p>
+                <p>
+                JS, or JavaScript, is a scripting language that adds interactive elements to HTML web pages that engage the user.  JS is embedded directly into HTML web pages and can update and change both HTML and CSS.  JS syntax involves two types of values, which are either fixed values or variable values.  Fixed values, or literals, are constant values that attach to specific variables.  Variable values, or variables, holds the data value, which varies over time.  In addition, JS also uses operators to compute and assign values, expressions which is a combination of values, variables, and operators to calculate a value, keywords to identify actions. Comments are not executed and are there for recordkeeping.  JS is the most functional programming language to use when making a web page interactive for its user. 
+                </p>
+                <p>
+                HTML, CSS, and JS all work together to create an engaging web page for browsers.  Each language has its purpose for the functionality of a website.  HTML provides structure, CSS provides stylization, and JS creates interactive content.
+                </p>
             </Essay>
         </div>
     );
