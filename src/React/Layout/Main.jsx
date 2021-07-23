9866a0df260ec38f64539f5eb0d93a0190d73d6a
Chris Pollock <cpollock911@gmail.com>
2021-07-22 18:32:42 -0700

    Added Foundation for Router and SPA

--- src/React/Layout/Main.jsx
+++ src/React/Layout/Main.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Switch, Route } from 'react-router-dom';
 
 import Homework from '../Homework/Homework.jsx';
-import SunAndMoon from '../SunAndMoon/SunAndMoon.jsx';
 
 const Main = () => {
     return (
         <MainStyled>
             <h1>Main Content</h1>
-            <SunAndMoon/>
-            <Homework/>
+           <Switch>
+               <Route path='/services'>
+                   Services
+                </Route>
+                <Route path='/contact'>
+                   Contact
+                </Route>
+                <Route path='/login'>
+                   Login
+                </Route>
+                <Route path='/homework'>
+                   <Homework />
+                </Route>
+                <Route path='/'>
+                   Welcome
+                </Route>
+           </Switch>
         </MainStyled>    
     ) 
 }
